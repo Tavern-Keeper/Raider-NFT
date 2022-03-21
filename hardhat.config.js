e80814004d410a0b33b6d547895cda7d4cad5429
Ryan <81717668+RyanSea@users.noreply.github.com>
2022-03-20 17:57:11 -0700

    Update Hardhat Config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,23 @@
 require("@nomiclabs/hardhat-waffle");
 
-const config = require('./config/config.json') //Alchemy URL and Private Key in config folder
+const {polygon, alchemy, privateKey} = require('./config/config.json') //Alchemy URL and Private Key in config folder
 
 
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   networks: {
+    polygon_mumbai: {
+      url: polygon,
+      accounts: [privateKey]
+    },
+    harmony_testnet: {
+      url: 'https://api.s0.pops.one/',
+      accounts: [privateKey]
+    },
     rinkeby: {
-      url: config.alchemy,
-      accounts: [config.privateKey]
+      url: alchemy,
+      accounts: [privateKey]
     }
-  }
-};
+  },
+}
